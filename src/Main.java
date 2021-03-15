164c2c122640861a757f74f554c79f335c1482b7
MechJM <up201703884@fe.up.pt>
2021-03-15 22:12:07 +0000

    Making progress with using gradle to build the project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
+import pt.up.fe.comp.jmm.JmmNode;
 import pt.up.fe.comp.jmm.JmmParser;
 import pt.up.fe.comp.jmm.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.specs.util.SpecsIo;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -13,8 +15,8 @@ public class Main implements JmmParser {
 	public JmmParserResult parse(String jmmCode) {
 		
 		try {
-		    Calculator myCalc = new Calculator(new StringReader(jmmCode));
-    		SimpleNode root = myCalc.Expression(); // returns reference to root node
+		    Parser myParser = new Parser(new StringReader(jmmCode));
+    		SimpleNode root = myParser.Program(); // returns reference to root node
             	
     		root.dump(""); // prints the tree on the screen
     	
@@ -25,7 +27,10 @@ public class Main implements JmmParser {
 	}
 
     public static void main(String[] args) {
-        System.out.println("Executing with args: " + Arrays.toString(args));
+		var main = new Main();
+		var fileContents = SpecsIo.read(args[0]);
+		main.parse(fileContents);
+
         if (args[0].contains("fail")) {
             throw new RuntimeException("It's supposed to fail");
         }
--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -11,9 +11,9 @@ import pt.up.fe.comp.TestUtils;
 public class ExampleTest {
 
 
-    @Test
-    public void testExpression() {		
-		assertEquals("Expression", TestUtils.parse("2+3\n").getRootNode().getKind());		
-	}
+  //   @Test
+  //   public void testExpression() {		
+	// 	assertEquals("Expression", TestUtils.parse("2+3\n").getRootNode().getKind());		
+	// }
 
 }
