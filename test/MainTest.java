848d883e83a2b534ef91df4456a6859d77b1168d
MechJM <up201703884@fe.up.pt>
2021-03-18 19:48:28 +0000

    Added fail tests

--- test/MainTest.java
+++ test/MainTest.java
@@ -86,4 +86,131 @@ public class MainTest {
         main.parse(fileContent);
     }
 
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail1()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/arr_index_not_int.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail2()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/arr_size_not_int.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail3()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/badArguments.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail4()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/binop_incomp.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail5()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/funcNotFound.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail6()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/simple_length.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail7()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/var_exp_incomp.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail8()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/var_lit_incomp.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail9()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/var_undef.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSemanticFail10()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/fail/semantic/varNotInit.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    @Test(expected = RuntimeException.class)
+    public void parseTestSyntacticalFail1()
+    {
+        String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/BlowUp.jmm");
+        Main main = new Main();
+        main.parse(fileContent);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseTestSyntacticalFail2()
+    {
+        String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/CompleteWhileTest.jmm");
+        Main main = new Main();
+        main.parse(fileContent);
+    }
+
+    // @Test(expected = RuntimeException.class)
+    // public void parseTestSyntacticalFail3()
+    // {
+    //     String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/LengthError.jmm");
+    //     Main main = new Main();
+    //     main.parse(fileContent);
+    // }
+
+    @Test(expected = RuntimeException.class)
+    public void parseTestSyntacticalFail4()
+    {
+        String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/MissingRightPar.jmm");
+        Main main = new Main();
+        main.parse(fileContent);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseTestSyntacticalFail5()
+    {
+        String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/MultipleSequential.jmm");
+        Main main = new Main();
+        main.parse(fileContent);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseTestSyntacticalFail6()
+    {
+        String fileContent = SpecsIo.read("test/fixtures/public/fail/syntactical/NestedLoop.jmm");
+        Main main = new Main();
+        main.parse(fileContent);
+    }
 }
