8ebbef2713e4e1c2d353c029c661310b3b9e0f50
up201806206@fe.up.pt <up201806206@fe.up.pt>
2021-04-13 04:46:14 +0100

    fixed method name function

--- src/ArithmeticVisitor.java
+++ src/ArithmeticVisitor.java
@@ -1,23 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import pt.up.fe.comp.jmm.JmmNode;
-import pt.up.fe.comp.jmm.analysis.table.Symbol;
-import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
 import pt.up.fe.comp.jmm.report.Report;
-import pt.up.fe.comp.jmm.report.ReportType;
-import pt.up.fe.comp.jmm.report.Stage;
 
-public class ArithmeticVisitor extends PreorderJmmVisitor<MySymbolTable,List<Report>> {
-    public ArithmeticVisitor()
-    {
+import java.util.List;
+
+public class ArithmeticVisitor extends PreorderJmmVisitor<MySymbolTable, List<Report>> {
+    public ArithmeticVisitor() {
         // addVisit("Add", this::processOperation);
         // addVisit("Sub", this::processOperation);
         // addVisit("Mul", this::processOperation);
         // addVisit("Div", this::processOperation);
     }
-    
+
     // List<Report> processOperation(JmmNode node, MySymbolTable table)
     // {
     //     List<Report> reports = new ArrayList<>();
@@ -56,19 +49,19 @@ public class ArithmeticVisitor extends PreorderJmmVisitor<MySymbolTable,List<Rep
     //             case "Method":
     //             {
     //                 var child = jmmNode.getChildren().get(1);
-                  
+
     //                 String methodName = child.get("name");
     //                 Report report = searchMethod(methodName, table);
     //                 if (report != null) reports.add(report);
-                    
+
     //             }
     //             case "VariableName":
     //             {
-                   
+
     //                 String methodName = getMethodName(node);
     //                 Report report = searchIdentifier(jmmNode.get("name"), methodName, table);
     //                 if (report != null) reports.add(report);
-                   
+
     //                 break;
     //             }
     //             default:
@@ -122,9 +115,13 @@ public class ArithmeticVisitor extends PreorderJmmVisitor<MySymbolTable,List<Rep
     //     return null;
     // }
 
-    // String getMethodName(JmmNode node)
-    // {
-    //     while (!(node.getParent().getKind().contains("MethodDeclaration"))) node = node.getParent();
-    //     return node.getKind();
-    // }
+    // String getMethodName(JmmNode node) {
+    //        while (true) {
+    //            if (node.getParent().getKind().contains("NormalMethodDeclaration")) {
+    //                return node.getParent().getChildren().get(1).get("name");
+    //            } else if (node.getParent().getKind().contains("MainMethodDeclaration"))
+    //                return "main";
+    //            node = node.getParent();
+    //        }
+    //    }
 }
