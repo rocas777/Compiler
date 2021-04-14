9b3dd1b4289f27261b31db67a2781eff1ae1387c
MechJM <up201703884@fe.up.pt>
2021-04-15 00:23:09 +0100

    Reverted file changes commited by mistake

--- test/MainTest.java
+++ test/MainTest.java
@@ -5,82 +5,78 @@ import pt.up.fe.specs.util.SpecsIo;
 
 public class MainTest {
 
-    // @Test
-    // public void parseTestPass1() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/FindMaximum.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+    @Test
+    public void parseTestPass1() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/FindMaximum.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
-    // @Test
-    // public void parseTestPass2() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/HelloWorld.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+    @Test
+    public void parseTestPass2() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/HelloWorld.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
-    // @Test
-    // public void parseTestPass3() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Lazysort.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+    @Test
+    public void parseTestPass3() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Lazysort.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
-    // @Test
-    // public void parseTestPass4() {
+    @Test
+    public void parseTestPass4() {
 
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Life.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Life.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
-    // @Test
-    // public void parseTestPass5() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/MonteCarloPi.jmm"));
-    //     TestUtils.noErrors(result.getReports());
+    @Test
+    public void parseTestPass5() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/MonteCarloPi.jmm"));
+        TestUtils.noErrors(result.getReports());
 
-    // }
+    }
 
-    // @Test
-    // public void parseTestPass6() {
+    @Test
+    public void parseTestPass6() {
 
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/QuickSort.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/QuickSort.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
-    // @Test
-    // public void parseTestPass7() {
+    @Test
+    public void parseTestPass7() {
 
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Simple.jmm"));
-    //     TestUtils.noErrors(result.getReports());
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/Simple.jmm"));
+        TestUtils.noErrors(result.getReports());
 
-    // }
+    }
 
-    // @Test
-    // public void parseTestPass8() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/TicTacToe.jmm"));
-    //     TestUtils.noErrors(result.getReports());
+    @Test
+    public void parseTestPass8() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/TicTacToe.jmm"));
+        TestUtils.noErrors(result.getReports());
 
-    // }
+    }
 
-    // @Test
-    // public void parseTestPass9() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/WhileAndIF.jmm"));
-    //     TestUtils.noErrors(result.getReports());
-    // }
+    @Test
+    public void parseTestPass9() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/WhileAndIF.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
 
 
-    // @Test
-    // public void parseTestSemanticFail1() {
-    //     var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/semantic/arr_index_not_int.jmm"));
-    //     var result2 = TestUtils.analyse(result);
-    //     TestUtils.mustFail(result2.getReports());
-    // }
+    @Test
+    public void parseTestSemanticFail1() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/semantic/arr_index_not_int.jmm"));
+        var result2 = TestUtils.analyse(result);
+        TestUtils.mustFail(result2.getReports());
+    }
 
     @Test
     public void parseTestSemanticFail2() {
         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/semantic/bool_op_incomp.jmm"));
         var result2 = TestUtils.analyse(result);
-        var reports = result2.getReports();
-        for (Report report : reports) {
-            System.out.println(report.toString());
-        }
         TestUtils.mustFail(result2.getReports());
     }
 
@@ -170,33 +166,33 @@ public class MainTest {
         TestUtils.mustFail(result.getReports());
     }*/
 
-//     @Test
-//     public void parseTestSyntacticalFail2() {
-//         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/CompleteWhileTest.jmm"));
-//         TestUtils.mustFail(result.getReports());
-//     }
+    @Test
+    public void parseTestSyntacticalFail2() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/CompleteWhileTest.jmm"));
+        TestUtils.mustFail(result.getReports());
+    }
 
-//     @Test
-//     public void parseTestSyntacticalFail3() {
-//         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/LengthError.jmm"));
-//         TestUtils.mustFail(result.getReports());
-//     }
+    @Test
+    public void parseTestSyntacticalFail3() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/LengthError.jmm"));
+        TestUtils.mustFail(result.getReports());
+    }
 
-//     @Test
-//     public void parseTestSyntacticalFail4() {
-//         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/MissingRightPar.jmm"));
-//         TestUtils.mustFail(result.getReports());
-//     }
+    @Test
+    public void parseTestSyntacticalFail4() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/MissingRightPar.jmm"));
+        TestUtils.mustFail(result.getReports());
+    }
 
-//     @Test
-//     public void parseTestSyntacticalFail5() {
-//         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/MultipleSequential.jmm"));
-//         TestUtils.mustFail(result.getReports());
-//     }
+    @Test
+    public void parseTestSyntacticalFail5() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/MultipleSequential.jmm"));
+        TestUtils.mustFail(result.getReports());
+    }
 
-//     @Test
-//     public void parseTestSyntacticalFail6() {
-//         var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/NestedLoop.jmm"));
-//         TestUtils.mustFail(result.getReports());
-//     }
+    @Test
+    public void parseTestSyntacticalFail6() {
+        var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/fail/syntactical/NestedLoop.jmm"));
+        TestUtils.mustFail(result.getReports());
+    }
 }
