9d2506a3ec40dc16c0a7c05fb6c9b8bddbbc4e45
MechJM <up201703884@fe.up.pt>
2021-04-15 13:53:42 +0100

    Working on checking arg and parameter types

--- src/FuncCallVisitor.java
+++ src/FuncCallVisitor.java
@@ -1,4 +1,5 @@
 import pt.up.fe.comp.jmm.JmmNode;
+import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
@@ -23,19 +24,72 @@ public class FuncCallVisitor extends PreorderJmmVisitor<MySymbolTable, Boolean> 
             var args = thirdChild.getChildren();
 
             if (parameters.size() != args.size()) Main.semanticReports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(secondChild.get("line")), Integer.parseInt(secondChild.get("column")), "Number of arguments in method call and number of parameters in declaration don't match"));
-            // else
-            // {
-            //     for (int i = 0; i < args.size(); i++) {
-            //         var currentParam = parameters.get(i);
-            //         var currentArg = args.get(i);
+            else
+            {
+                for (int i = 0; i < args.size(); i++) {
+                    var currentParam = parameters.get(i);
+                    var currentArg = args.get(i);
 
+                    Type paramType = currentParam.getType();
+                    Type argType;
+                    
+                    switch (currentArg.getKind())
+                    {
+                        case "Add":
+                        {
+                            argType = new Type("int", false);
+                            break;
+                        }
+                        case "Sub":
+                        {
+                            argType = new Type("int", false);
+                            break;
+                        }
+                        case "Mul":
+                        {
+                            argType = new Type("int", false);
+                            break;
+                        }
+                        case "Div":
+                        {
+                            argType = new Type("int", false);
+                            break;
+                        }
+                        case "IntegerLiteral":
+                        {
+                            argType = new Type("int", false);
+                            break;
+                        }
+                        case "LessThan":
+                        {
+                            argType = new Type("boolean", false);
+                            break;
+                        }
+                        case "AND":
+                        {
+                            argType = new Type("boolean", false);
+                            break;
+                        }
+                        case "Method":
+                        {
 
-            //     }
-            // }
+                        }
+                        default:
+                        {
+                            argType = null;
+                            break;
+                        }
+                    }
+
+                    //if (!argType.equals(paramType)) 
+                }
+            }
         
         }
 
         return true;
     }
-    
+
+
+    //function for finding the first descendant with a line and column
 }
