b56c2a329c95b28c5be534b5ed6ddbcc00525a48
MechJM <up201703884@fe.up.pt>
2021-05-31 00:47:09 +0100

    Updated ioPlus src file
    Former-commit-id: f77bd389b0c9da58bdce50bad7137b1d26a5bf1f

--- test/fixtures/libs/java/ioPlus.java
+++ test/fixtures/libs/java/ioPlus.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ioPlus {
     public static void printHelloWorld()
     {
@@ -7,5 +11,19 @@ public class ioPlus {
     public static void printResult(int num)
     {
         System.out.println(num);
+    }
+
+    public static int requestNumber()
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try
+        {
+            String input = reader.readLine();
+            return Integer.parseInt(input);
+        }
+        catch (IOException ignored)
+        {
+            return 0;
+        }
     }
 }
